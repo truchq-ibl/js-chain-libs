3347c0e7ee59469c6a1eec1731dcf78df7075f62
truchq-ibl <33276947+truchq-ibl@users.noreply.github.com>
2019-12-16 17:16:16 +0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -856,7 +856,7 @@ impl From<tx::UtxoPointer> for UtxoPointer {
 #[wasm_bindgen]
 impl UtxoPointer {
     pub fn new(fragment_id: &FragmentId, output_index: u8, value: &Value) -> UtxoPointer {
-        let a = "4d972cdc7b95ec0f16c53d56728a9c65de6c94302c8525b73ef6f785353b3898"
+        let a = "4d972cdc7b95ec0f16c53d56728a9c65de6c94302c8525b73ef6f785353b3898";
         UtxoPointer(tx::UtxoPointer {
             transaction_id: a.as_bytes(),
             output_index,
